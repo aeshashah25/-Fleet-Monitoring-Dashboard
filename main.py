e87e776e2b4c4d4d80f9fb0b87ed4137ee0910e1
aesha <aeshashah98416@gmail.com>
2024-12-18 19:16:03 +0530

    add main.py

--- main.py
+++ main.py
@@ -67,3 +67,4 @@ async def websocket_endpoint(websocket: WebSocket):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Get the dynamic port if it's available
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
